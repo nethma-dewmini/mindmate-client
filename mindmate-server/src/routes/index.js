d316c67bd265648ffd1d33461587877df4cd91db
nethma-dewmini <rathnayakarmnd.22@uom.lk>
2026-05-11 19:17:29 +0530

    created the get experts api

--- mindmate-server/src/routes/index.js
+++ mindmate-server/src/routes/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const healthRouter = require("./health.routes");
+const expertsRouter = require("./experts.routes");
 
 const router = express.Router();
 
 router.use("/health", healthRouter);
+router.use("/experts", expertsRouter);
 
 module.exports = router;
